79fa8708cdc447595d119d06af41efa04073969e
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-06-05 18:11:20 -0400

    FrameRunner now takes a type argument instead of using a trait obj.

--- src/runner.rs
+++ src/runner.rs
@@ -2,13 +2,15 @@ use clock::{GameTime, GameClock};
 use framerate::counter::FrameCount;
 
 #[derive(Debug)]
-pub struct FrameRunner {
+pub struct FrameRunner<C: FrameCount> {
     clock: GameClock,
-    counter: Box<FrameCount>,
+    counter: C,
 }
 
-impl FrameRunner {
-    pub fn new(clock: GameClock, counter: Box<FrameCount>) -> FrameRunner {
+impl<C> FrameRunner<C>
+    where C: FrameCount
+{
+    pub fn new(clock: GameClock, counter: C) -> FrameRunner<C> {
         FrameRunner {
             clock: clock,
             counter: counter,
@@ -21,14 +23,14 @@ impl FrameRunner {
     pub fn clock_mut(&mut self) -> &mut GameClock {
         &mut self.clock
     }
-    pub fn counter(&self) -> &FrameCount {
-        self.counter.as_ref()
+    pub fn counter(&self) -> &C {
+        &self.counter
     }
-    pub fn counter_mut(&mut self) -> &mut FrameCount {
-        self.counter.as_mut()
+    pub fn counter_mut(&mut self) -> &mut C {
+        &mut self.counter
     }
     pub fn tick(&mut self) -> GameTime {
-        let time = self.clock.tick(self.counter.as_mut());
+        let time = self.clock.tick(&mut self.counter);
         time
     }
 }
