5dc18df8f2aaca4d05b991fe852cf27c78efea6e
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-06-06 13:38:31 -0400

    Remove error module.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@ extern crate time;
 extern crate float_duration;
 
 pub mod clock;
-pub mod error;
 pub mod framerate;
 pub mod runner;
 
 pub use self::clock::{GameTime, GameClock, TimeProgression};
-pub use self::error::DurationError;
 pub use self::framerate::{FrameCounter, FrameCount, FrameRateSampler};
 pub use self::runner::FrameRunner;
