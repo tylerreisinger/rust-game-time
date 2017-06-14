53f83d2b5c72ba3a7c10f151e25d3f8f2cf847d5
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-06-14 15:39:08 -0400

    Document `time_progression` module.

--- src/time_progression.rs
+++ src/time_progression.rs
@@ -8,18 +8,22 @@ pub trait TimeProgression {
     fn compute_game_time(&mut self, wall_time: &FloatDuration) -> FloatDuration;
 }
 
+/// A time step based on elapsed wall time.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct VariableStep {}
+/// A fixed time step derived from a set frame rate.
 #[derive(Debug)]
 pub struct FixedStep<'a, C: 'a + FrameCount + ?Sized> {
     counter: &'a mut C,
 }
+/// A specific, constant time step.
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct ConstantStep {
     step: FloatDuration,
 }
 
 impl VariableStep {
+    /// Construct a new `VariableStep` object.
     pub fn new() -> VariableStep {
         VariableStep {}
     }
@@ -27,11 +31,13 @@ impl VariableStep {
 impl<'a, C> FixedStep<'a, C>
     where C: 'a + FrameCount + ?Sized
 {
+    /// Construct a new `FixedStep` object based on the set frame rate in `counter`.
     pub fn new(counter: &'a mut C) -> FixedStep<'a, C> {
         FixedStep { counter: counter }
     }
 }
 impl ConstantStep {
+    /// Construct a new `ConstantStep` object with a set time step `step`.
     pub fn new(step: FloatDuration) -> ConstantStep {
         ConstantStep { step }
     }
