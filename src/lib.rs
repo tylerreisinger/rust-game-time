bfe2a3370d20925add1fc6c956fc0b57caea63ce
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-06-14 16:05:55 -0400

    Re-export `TimeProgression` trait.

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,4 @@ pub mod progression;
 pub use self::clock::{GameTime, GameClock};
 pub use self::framerate::{FrameCounter, FrameCount, FrameRateSampler};
 pub use self::runner::FrameRunner;
+pub use self::progression::TimeProgression;
